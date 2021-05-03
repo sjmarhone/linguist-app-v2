6b35fb40d0bc749b719b5cc00b1839de166edf44
Sarahjean Marhone <sjmarhone@gmail.com>
2021-05-03 10:13:38 -0400

    deply commit

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    // <nav>
-    //   <div>
-    //     <span>Linguist</span>
-    //   </div>
-    // </nav>
-
-    <nav className="navbar navbar-light bg-light">
-      <div className="container-fluid">
-        <span className="navbar-text">Linguist</span>
+    <nav>
+      <div>
+        <span>Linguist</span>
       </div>
     </nav>
+
+    // <nav className="navbar navbar-light bg-light">
+    //   <div className="container-fluid">
+    //     <span className="navbar-text">Linguist</span>
+    //   </div>
+    // </nav>
   );
 };
 
